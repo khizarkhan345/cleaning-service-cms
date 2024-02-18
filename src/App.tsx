74353e359f118fb86310d9702c41a18416271778
Khizar Khan <khezarhayat881@gmail.com>
2024-02-17 19:46:59 -0500

    created the order change page UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TopBar from "./components/TopBar/TopBar";
 import SideBar from "./components/SideBar/SideBar";
 import DataTable from "./components/OrderTable/OrderTable";
 import OrderDetails from "./components/OrderDetails/OrderDetails";
+import ChangeOrderDetails from "./components/ChangeOrderDetails/ChangeOrderDetails";
 
 import "./App.css";
 
@@ -12,7 +13,7 @@ function App() {
       <TopBar />
       <div className="flex flex-row">
         <SideBar />
-        <OrderDetails />
+        <ChangeOrderDetails />
       </div>
     </div>
   );
--- src/components/ChangeOrderDetails/ChangeOrderDetails.tsx
+++ src/components/ChangeOrderDetails/ChangeOrderDetails.tsx
@@ -6,51 +6,63 @@ const ChangeOrderDetails = () => {
       <div className="grid grid-cols-2 justify-items-start mb-[50px]">
         <div className="flex flex-col">
           <span className="text-[20px] text-[#636D84] font-medium">Name</span>
-          <span className="text-[24px] font-semibold mt-[10px]">
-            Khizar Khan
-          </span>
+          <input
+            type="text"
+            value="Khizar Khan"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
         <div className="flex flex-col pl-[100px]">
           <span className="text-[20px] text-[#636D84] font-medium">
             Address
           </span>
-          <span className="text-[24px] font-semibold  mt-[10px]">
-            123 XYZ Lorton VA 22079
-          </span>
+          <input
+            type="text"
+            value="XYZ Lorton VA 22079"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
       </div>
       <div className="grid grid-cols-2 justify-items-start mb-[50px]">
         <div className="flex flex-col">
           <span className="text-[20px] text-[#636D84] font-medium">Email</span>
-          <span className="text-[24px] font-semibold mt-[10px]">
-            khizarkhan@test.com
-          </span>
+          <input
+            type="text"
+            value="khizar@test.com"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
         <div className="flex flex-col pl-[100px]">
           <span className="text-[20px] text-[#636D84] font-medium">
             Phone No
           </span>
-          <span className="text-[24px] font-semibold  mt-[10px]">
-            888-123-4567
-          </span>
+          <input
+            type="text"
+            value="888-444-2345"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
       </div>
       <div className="grid grid-cols-2 justify-items-start mb-[50px]">
         <div className="flex flex-col">
           <span className="text-[20px] text-[#636D84] font-medium">
-            Created Date
+            Delivery Date
           </span>
-          <span className="text-[24px] font-semibold mt-[10px]">
-            February 15th 2024
-          </span>
+          <input
+            type="date"
+            value="02/15/2024"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
         <div className="flex flex-col pl-[100px]">
           <span className="text-[20px] text-[#636D84] font-medium">
-            Delivery Date & Time
+            Delivery Time
           </span>
-          <span className="text-[24px] font-semibold  mt-[10px]">
-            February 28th 2024 14:00
-          </span>
+          <input
+            type="text"
+            value="13:00"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
       </div>
       <div className="grid grid-cols-2 justify-items-start mb-[50px]">
@@ -58,13 +70,21 @@ const ChangeOrderDetails = () => {
           <span className="text-[20px] text-[#636D84] font-medium">
             No Of Bedrooms
           </span>
-          <span className="text-[24px] font-semibold mt-[10px]">1</span>
+          <input
+            type="number"
+            value="1"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
         <div className="flex flex-col pl-[100px]">
           <span className="text-[20px] text-[#636D84] font-medium">
             Number of Bathrooms
           </span>
-          <span className="text-[24px] font-semibold  mt-[10px]">1</span>
+          <input
+            type="number"
+            value="1"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
       </div>
       <div className="grid grid-cols-2 justify-items-start mb-[50px]">
@@ -72,13 +92,21 @@ const ChangeOrderDetails = () => {
           <span className="text-[20px] text-[#636D84] font-medium">
             No Of LivingRooms
           </span>
-          <span className="text-[24px] font-semibold mt-[10px]">2</span>
+          <input
+            type="number"
+            value="2"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
         <div className="flex flex-col pl-[100px]">
           <span className="text-[20px] text-[#636D84] font-medium">
             Number of Kitchens
           </span>
-          <span className="text-[24px] font-semibold  mt-[10px]">0</span>
+          <input
+            type="number"
+            value="0"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
       </div>
       <div className="grid grid-cols-2 justify-items-start mb-[50px]">
@@ -86,20 +114,26 @@ const ChangeOrderDetails = () => {
           <span className="text-[20px] text-[#636D84] font-medium">
             Total Cost
           </span>
-          <span className="text-[24px] font-semibold mt-[10px]">$200</span>
+          <input
+            type="text"
+            value="$200"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
         <div className="flex flex-col pl-[100px]">
           <span className="text-[20px] text-[#636D84] font-medium">
             Order Status
           </span>
-          <span className="text-[24px] font-semibold  mt-[10px]">
-            Created/Pending/Completed
-          </span>
+          <input
+            type="text"
+            value="Pending/Completed"
+            className="w-[300px] h-[55px] border-2 dark:border-gray-700 rounded-[15px] pl-[20px] focus:outline-none"
+          />
         </div>
       </div>
       <div className="flex justify-center">
         <button className="bg-black text-[24px] font-bold w-[320px] h-[62px] text-white rounded-[15px] mx-auto">
-          Change Order
+          Submit
         </button>
       </div>
     </div>
