6104a1a28384f8e7862eef62ee3d55062510adab
Khizar Khan <khezarhayat881@gmail.com>
2024-02-20 16:49:34 -0500

    add the Pagination feature

--- src/Types/Types.ts
+++ src/Types/Types.ts
@@ -13,6 +13,8 @@ interface customer {
     updatedAt: string,
 }
 export interface orders {
+    length: number
+    slice(startIndex: number, endIndex: number): unknown
     map(arg0: (d: orders) => import("react/jsx-runtime").JSX.Element): import("react").ReactNode
     id: string,
     noOfBedrooms: string,
@@ -26,4 +28,11 @@ export interface orders {
     createdAt: string,
     updatedAt: string
     customer: customer
+}
+
+export interface pagination {
+    currentPage: number,
+    itemsPerPage: number,
+    noOfPages: number,
+    handlePageChange: (page: number) => void
 }
